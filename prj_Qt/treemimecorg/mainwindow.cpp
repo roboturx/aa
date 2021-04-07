c0579044d17f45b4edcb3c7a6b42006fe86887a3
roboturx <roboturx@gmail.com>
2021-04-07 18:08:55 +0300

    aaaaaaaaaaa

--- prj_Qt/treemimecorg/mainwindow.cpp
+++ prj_Qt/treemimecorg/mainwindow.cpp
@@ -3,7 +3,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "sqltreemodel.h"
-
+#include "database.h"
 #include <QSqlQueryModel>
 
 
@@ -12,14 +12,23 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
-    db= QSqlDatabase::database();
+    DataBase db;
+    
+    //db= QSqlDatabase::database();
     QSqlQueryModel *model1 = new QSqlQueryModel(this);
-    model1->setQuery("SELECT id_category, name FROM category ca WHERE ca.id_parent=0",db);
+    // model1->setQuery("SELECT id_category, name
+    //                      FROM category ca
+    //                      WHERE ca.id_parent=0",db);
+    model1->setQuery("SELECT h_kod,h_ad,h_acklm FROM h_snf "
+                     "WHERE h_prnt = 0",db);
     SqlTreeModel *pepe = new SqlTreeModel();
     pepe->appendModel(model1);
     QSqlQueryModel *model = new QSqlQueryModel(this);
-    model->setQuery("SELECT id_category, id_parent, name FROM category ca WHERE ca.id_parent=1",db);
+    // model->setQuery("SELECT id_category, id_parent, name "
+    //                 "FROM category ca
+    //                    WHERE ca.id_parent=1",db);
+    model->setQuery("SELECT h_kod, h_prnt, h_ad, h_acklm "
+                    "FROM h_grp WHERE h_prnt=1",db);
     pepe->appendModel(model);;
     ui->treeView->setModel(pepe);
 
--- prj_Qt/treemimecorg/treemimecorg.pro
+++ prj_Qt/treemimecorg/treemimecorg.pro
@@ -9,11 +9,13 @@ CONFIG += c++11
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
+    database.cpp \
     main.cpp \
     mainwindow.cpp \
     sqltreemodel.cpp
 
 HEADERS += \
+    database.h \
     mainwindow.h \
     sqltreemodel.h
 
