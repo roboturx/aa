13ac63d85f7de6cb5b795c208812abbf20dfcb9d
roboturx <roboturx@gmail.com>
2021-12-16 17:13:24 +0300

    committing

--- prj_Qt/aa2/main/arayuz.cpp
+++ prj_Qt/aa2/main/arayuz.cpp
@@ -1,19 +1,163 @@
 #include "globals.h"
 #include "arayuz.h"
-#include "ui_arayuz.h"
+
 
 Arayuz::Arayuz(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::Arayuz)
+    QWidget(parent)
 {
-    ui->setupUi(this);
+
 //    dbase=new DBase;
 //    dbase->setGeometry (20,30,300,480);
 //    dbase->setWindowTitle("Veri Tabanı Kontrol");
 //    dbase->show();
+    duzenle = new Duzenle;
+
+    //this->adjustSize();
+    auto g_lytara = new QGridLayout();
+    this->setLayout(g_lytara);
+    g_lytara->addWidget(duzenle, 0, 0, 1, 1);
+
 }
 
 Arayuz::~Arayuz()
 {
-    delete ui;
+
 }
+
+// ----------------------------------- düzenleme tarih - no
+Duzenle::Duzenle(QWidget *parent) :
+    QWidget(parent)
+{
+    auto *x1 = new QHBoxLayout ;
+    auto *lB_duztar = new QLabel("Sözleşmenin Düzenleme &Tarihi",this) ;
+    dE_duztar = new QDateEdit(this);
+    lB_duztar->setBuddy(dE_duztar);
+    x1->addWidget(lB_duztar);
+    x1->addWidget(dE_duztar);
+
+
+    auto *x2 = new QHBoxLayout ;
+    auto *lB_duzno = new QLabel("Düzenleme &No",this) ;
+    lE_duzno = new QLineEdit(this);
+    lB_duzno->setBuddy(lE_duzno);
+    x2->addWidget(lB_duzno);
+    x2->addWidget(lE_duzno);
+
+   // this->adjustSize();
+    auto g_lytduz = new QGridLayout;
+    this->setLayout(g_lytduz);
+
+    g_lytduz->setColumnMinimumWidth(0,20);
+    g_lytduz->setColumnStretch(0,0);
+    g_lytduz->setColumnMinimumWidth(1,100);
+    g_lytduz->setColumnStretch(1,0);
+    g_lytduz->setColumnMinimumWidth(2,20);
+    g_lytduz->setColumnStretch(2,0);
+    g_lytduz->setColumnMinimumWidth(3,100);
+    g_lytduz->setColumnStretch(3,0);
+    g_lytduz->setColumnMinimumWidth(4,100);
+    g_lytduz->setColumnStretch(4,0);
+
+
+    g_lytduz->addLayout(x1, 0, 0, 1, 1);
+    //g_lytduz->addWidget(dE_duztar, 0, 4, 1, 2);
+    g_lytduz->addLayout(x2, 0, 2, 1, 1);
+    //g_lytduz->addWidget(lE_duzno,  0, 8, 1, 2);
+
+
+
+}
+
+Duzenle::~Duzenle()
+{
+
+}
+
+
+/// ----------------------------------- kiraya veren
+/// kiraya veren
+///
+/// tc
+/// ad soyad
+/// adres
+/// telefon
+///
+Kveren::Kveren(QWidget *parent) :
+    QWidget(parent)
+{
+
+}
+
+Kveren::~Kveren()
+{
+
+}
+
+
+/// ----------------------------------- kiracı
+/// kiracı
+///
+/// tc
+/// ad soyad
+/// adres
+/// telefon
+///
+
+Kirac::Kirac(QWidget *parent) :
+    QWidget(parent)
+{
+
+}
+
+Kirac::~Kirac()
+{
+
+}
+
+
+/// ----------------------------------- mecur
+/// kiralanan yer
+///
+/// adres kodu
+/// adres
+/// tapu pafta ada parsel
+/// abone su elektrik doğalgaz
+///
+
+
+// -----------------------------------
+Kiralanan::Kiralanan(QWidget *parent) :
+    QWidget(parent)
+{
+
+}
+
+Kiralanan::~Kiralanan()
+{
+
+}
+
+
+/// -----------------------------------  kira
+/// kiranın
+///
+/// başlangıç tarihi
+/// bitiş tarihi
+/// süresi
+/// bedeli
+/// teminat bedeli
+/// ödeme şekli
+/// ödeme yeri
+///
+Kira::Kira(QWidget *parent) :
+    QWidget(parent)
+{
+
+}
+
+Kira::~Kira()
+{
+
+}
+
+
--- prj_Qt/aa2/main/arayuz.h
+++ prj_Qt/aa2/main/arayuz.h
@@ -3,10 +3,101 @@
 
 #include "globals.h"
 #include "dbase.h"
-namespace Ui {
+namespace ns_arayuz {
 class Arayuz;
+class Duzenle;
+class Kveren;
+class Kirac;
+class Kiralanan;
+class Kira;
 }
 
+
+//----------------------------------------------  duzenle
+class Duzenle : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Duzenle(QWidget *parent = nullptr);
+    ~Duzenle();
+
+
+private:
+    //QLabel lB_dtar;
+    QDateEdit *dE_duztar;
+    QLineEdit *lE_duzno;
+};
+
+//----------------------------------------------  kveren
+class Kveren : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Kveren(QWidget *parent = nullptr);
+    ~Kveren();
+
+
+private:
+    QLabel lB_tc;
+    QLineEdit lE_tc;
+    QLabel lB_ad;
+    QLineEdit lE_ad;
+    QLabel lB_syd;
+    QLineEdit lE_syd;
+    QLabel lB_adres;
+    QLineEdit lE_adres;
+    QLabel lB_tel;
+    QLineEdit lE_tel;
+
+};
+
+//----------------------------------------------  kirac
+class Kirac : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Kirac(QWidget *parent = nullptr);
+    ~Kirac();
+
+
+private:
+
+};
+
+
+
+//----------------------------------------------  kiralanan
+class Kiralanan : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Kiralanan(QWidget *parent = nullptr);
+    ~Kiralanan();
+
+
+private:
+
+};
+
+//---------------------------------------------- kira
+class Kira : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Kira(QWidget *parent = nullptr);
+    ~Kira();
+
+
+private:
+
+};
+
+//---------------------------------------------- arayuz
 class Arayuz : public QWidget
 {
     Q_OBJECT
@@ -15,9 +106,12 @@ public:
     explicit Arayuz(QWidget *parent = nullptr);
     ~Arayuz();
 
-   DBase* dbase;
+
 private:
-    Ui::Arayuz *ui;
+    Duzenle *duzenle;
+
 };
 
+
 #endif // ARAYUZ_H
+
