132f5c7f35b0c16c4e4376e28e4ed2660f7ff647
roboturx <roboturx@gmail.com>
2022-03-12 18:17:33 +0300

    committing

---
+++ prj_Qt/treeview/tree2022/mainwindow.h
@@ -0,0 +1,28 @@
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include "ui_mainwindow.h"
+
+
+#include <QMainWindow>
+
+class MainWindow : public QMainWindow, private Ui::MainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+
+public slots:
+    void updateActions();
+
+private slots:
+    void insertChild();
+    bool insertColumn();
+    void insertRow();
+    bool removeColumn();
+    void removeRow();
+};
+
+#endif // MAINWINDOW_H
