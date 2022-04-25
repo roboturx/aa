1a9f7586759c69f33fda9e4bc57a5bdac72774c7
roboturx <roboturx@gmail.com>
2022-04-25 04:42:48 +0300

    committing

--- prj_Qt/treeEyllanesc/hc_treemodel.cpp
+++ prj_Qt/treeEyllanesc/hc_treemodel.cpp
@@ -3,27 +3,26 @@
 #include "dbase.h"
 #include <QTreeView>
 #include <QGridLayout>
+#include <QSortFilterProxyModel>
 //hC_TreeModel::hC_TreeModel(QObject *parent)
 //    : QStandardItemModel{parent}
 hC_TreeModel::hC_TreeModel()
 
 {
-    modelle();
-}
-
-QStandardItemModel *hC_TreeModel::modelle()
-{
+    //modelle();
     dBase db;
+    db.open ();
+
     stdmodel = new QStandardItemModel;
     QSqlQuery  query("SELECT GroupCode, AcName, ActCod from adm_ac "
-                     "ORDER BY ActCod ASC ");
+                    "ORDER BY ActCod ASC ");
 
     if (query.isActive())
     {  qDebug()<<"q is active"; }
-    else { qDebug()<<"q is active"; }
-    qDebug()<<"11111111";
+    else { qDebug()<<"q is NOT active"; }
+
     const QSqlRecord rec = query.record();
-    qDebug()<<"22222222";
+
     while (query.next())
     {
 
@@ -46,22 +45,77 @@ QStandardItemModel *hC_TreeModel::modelle()
                 parent->appendRow(it);
             }
         }
-            qDebug()<<"33333 "<< AcName <<" code role : "<< RelationRoles::CodeRole;
+        qDebug()<<"33333 "<< AcName <<" code role : "<< RelationRoles::CodeRole;
     }
 
+    QSortFilterProxyModel proxymodel;
+    // proxymodel.setSourceModel(treemodel);
+    QTreeView *w =new QTreeView;
 
-    QTreeView w;
-    //  QStandardItemModel* mystdmodel=modelle ();
-    qDebug()<< &stdmodel;
-    w.setModel(stdmodel);
-    w.setWindowTitle ("mainwind treemodel");
-    w.expandAll();
+    w->setModel(stdmodel);
+    w->setWindowTitle ("mainwind treemodel");
+    // w->expandAll();
     QWidget *aa = new QWidget();
 
     QGridLayout aal(aa);
-    aal.addWidget (&w,0,0);
-   // setCentralWidget (aa);
+    aal.addWidget (w,0,0);
+    // setCentralWidget (aa);
     aa->show ();
+}
+
+QStandardItemModel *hC_TreeModel::modelle()
+{
+//    dBase db;
+//    db.open ();
+
+//    stdmodel = new QStandardItemModel;
+//    QSqlQuery  query("SELECT GroupCode, AcName, ActCod from adm_ac "
+//                     "ORDER BY ActCod ASC ");
+
+//    if (query.isActive())
+//    {  qDebug()<<"q is active"; }
+//    else { qDebug()<<"q is active"; }
+//    qDebug()<<"11111111";
+//    const QSqlRecord rec = query.record();
+
+//    while (query.next())
+//    {
+// qDebug()<<"22222222";
+//        QString AcName = query.value(rec.indexOf("AcName")).toString();
+//        int GroupCode = query.value(rec.indexOf("GroupCode")).toInt();
+//        int ActCod = query.value(rec.indexOf("ActCod")).toInt();
+//        QStandardItem *it = new QStandardItem(AcName);
+//        it->setData(ActCod, RelationRoles::CodeRole);
+//        if(GroupCode == 0)
+//            stdmodel->invisibleRootItem()->appendRow(it);
+//        else
+//        {
+//            QModelIndexList ixs = stdmodel->match(stdmodel->index(0, 0),
+//                                                  RelationRoles::CodeRole,
+//                                                  GroupCode,
+//                                                  1,
+//                                                  Qt::MatchExactly| Qt::MatchRecursive);
+//            if(ixs.size() > 0){
+//                QStandardItem *parent = stdmodel->itemFromIndex(ixs.first());
+//                parent->appendRow(it);
+//            }
+//        }
+//            qDebug()<<"33333 "<< AcName <<" code role : "<< RelationRoles::CodeRole;
+//    }
+
+//    QSortFilterProxyModel proxymodel;
+//    // proxymodel.setSourceModel(treemodel);
+//    QTreeView *w =new QTreeView;
+
+//    w->setModel(stdmodel);
+//    w->setWindowTitle ("mainwind treemodel");
+//   // w->expandAll();
+//    QWidget *aa = new QWidget();
+
+//    QGridLayout aal(aa);
+//    aal.addWidget (w,0,0);
+//   // setCentralWidget (aa);
+//    aa->show ();
 
     return stdmodel;
 }
--- prj_Qt/treeEyllanesc/mainwindow.cpp
+++ prj_Qt/treeEyllanesc/mainwindow.cpp
@@ -1,28 +1,15 @@
 #include "mainwindow.h"
-#include <QSortFilterProxyModel>
+
 #include <QTableView>
 #include <QGridLayout>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
-    //db = new dBase ;
-   // treemodel = new hC_TreeModel;
 
-    QSortFilterProxyModel proxymodel;
-   // proxymodel.setSourceModel(treemodel);
-    QTreeView w;
-  //  QStandardItemModel* mystdmodel=modelle ();
-    qDebug()<< &stdmodel;
-    w.setModel(stdmodel);
-      w.setWindowTitle ("mainwind treemodel");
-    w.expandAll();
-      QWidget *aa = new QWidget(this);
+    treemodel = new hC_TreeModel;
 
-    QGridLayout aal(aa);
-      aal.addWidget (&w,0,0);
-    setCentralWidget (aa);
-      aa->show ();
+
 }
 
 MainWindow::~MainWindow()
