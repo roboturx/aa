714021146b74622d596ddbe1a98abbcdfa14da1e
roboturx <roboturx@gmail.com>
2022-05-18 17:02:03 +0300

    committing

--- prj_Qt/treeEyllanesc/mainwindow.cpp
+++ prj_Qt/treeEyllanesc/mainwindow.cpp
@@ -12,35 +12,34 @@ MainWindow::MainWindow(QWidget *parent)
     this->setGeometry(10,10,800,640);
 
 
-    qDebug()<<"66666";
+    qDebug()<<"Main";
     mysqlmodel = new MySqlModel(this);
-    qDebug()<<"7777";
+    qDebug()<<"MyModel";
     QTreeView *soltreeview =new QTreeView(this);
     soltreeview->setModel(mysqlmodel/*->stdmodel*/);
-    qDebug()<<"444";
     soltreeview->expandAll();
 
-    QSplitter* splitter =new QSplitter(this);
 
     QWidget *solwdgt = new QWidget(this);
     QGridLayout *sollyt = new QGridLayout(solwdgt);
     solwdgt->setLayout(sollyt);
-    qDebug()<<"55555";
     sollyt->addWidget(new QLabel ("Accounting") ,0,0);
     sollyt->addWidget(soltreeview,1,0);
+    qDebug()<<"SolTreeView";
 
+    treemodel = new hC_TreeModel;
+    qDebug()<<"hctreemodel";
 
-
-        treemodel = new hC_TreeModel;
     QTreeView *sagwdgt =new QTreeView(this);
     sagwdgt->setModel(proxymodel);
     proxymodel->setSortRole (Qt::DisplayRole);
     proxymodel->setFilterRegularExpression(
                 QRegularExpression("*", QRegularExpression::CaseInsensitiveOption));
     proxymodel->setFilterKeyColumn(0);
-
     sagwdgt->expandAll();
+    qDebug()<<"SağTreeView";
 
+    QSplitter* splitter =new QSplitter(this);
     splitter->addWidget (solwdgt);
     splitter->addWidget (sagwdgt);
     setCentralWidget (splitter);
